00ad96d011ca184fd20108dad1febc77a9b9c472
Smit Patel <smitpatel@users.noreply.github.com>
2017-01-19 17:02:08 -0800

    Query: Use discriminator type to create the discriminator constant values

--- src/Microsoft.EntityFrameworkCore.Relational/Query/Internal/RelationalResultOperatorHandler.cs
+++ src/Microsoft.EntityFrameworkCore.Relational/Query/Internal/RelationalResultOperatorHandler.cs
@@ -642,7 +642,7 @@ namespace Microsoft.EntityFrameworkCore.Query.Internal
                         .Select(concreteEntityType =>
                             Expression.Equal(
                                 discriminatorColumn,
-                                Expression.Constant(relationalMetadataExtensionProvider.For(concreteEntityType).DiscriminatorValue)))
+                                Expression.Constant(relationalMetadataExtensionProvider.For(concreteEntityType).DiscriminatorValue, discriminatorColumn.Type)))
                         .Aggregate((current, next) => Expression.OrElse(next, current));
 
                 handlerContext.SelectExpression.Predicate
